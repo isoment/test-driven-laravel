53ab7808c052d35b67cf36cf857fabc991ea0a8b
twill <twill1@gmx.com>
2022-07-05 19:17:25 -0500

    Section05.07

--- app/Reservation.php
+++ app/Reservation.php
@@ -5,12 +5,18 @@ declare(strict_types=1);
 namespace App;
 
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Collection as SupportCollection;
 
 class Reservation
 {
-    private Collection $tickets;
+    private SupportCollection|Collection $tickets;
 
-    public function __construct(Collection $tickets)
+    /**
+     *  We need to set this as a union type since for the test we are stubbing the eloquent
+     *  collection with just a plain collection
+     *  @param SupportCollection|Collection
+     */
+    public function __construct(SupportCollection|Collection $tickets)
     {
         $this->tickets = $tickets;
     }
