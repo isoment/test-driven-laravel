dce0537249a3b643f45407286e5f580942535696
twill <twill1@gmx.com>
2022-07-27 23:13:00 -0500

    Section09.03

---
+++ app/Billing/StripePaymentGateway.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Billing;
+
+class StripePaymentGateway implements PaymentGateway
+{
+    private string $apiKey;
+
+    public function __construct($apiKey)
+    {
+        $this->apiKey = $apiKey;
+    }
+
+    public function charge(int $amount, string $token): void
+    {
+        $stripe = new \Stripe\StripeClient();
+
+        $stripe->charges->create([
+            'amount' => $amount,
+            'currency' => 'usd',
+            'source' => $token
+        ], ['api_key' => $this->apiKey]);
+    }
+}
--- tests/Unit/Billing/StripePaymentGatewayTest.php
+++ tests/Unit/Billing/StripePaymentGatewayTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit\Billing;
 
+use App\Billing\StripePaymentGateway;
 use Tests\TestCase;
 
 class StripePaymentGatewayTest extends TestCase
@@ -12,7 +13,7 @@ class StripePaymentGatewayTest extends TestCase
     public function charges_with_a_valid_payment_token_are_successful()
     {
         // Create a new stripe payment gateway instance
-        $paymentGateway = new StripePaymentGateway;
+        $paymentGateway = new StripePaymentGateway(config('services.stripe.secret'));
 
         $stripe = new \Stripe\StripeClient();
 
@@ -23,17 +24,17 @@ class StripePaymentGatewayTest extends TestCase
                 'exp_year' => date('Y') + 1,
                 'cvc' => '123',
             ],
-        ], ['api_key' => config('services.stripe.secret')]);
+        ], ['api_key' => config('services.stripe.secret')])->id;
 
         // Create a new charge for an amount using a valid token
-        $paymentGateway->charge(2500, 'valid-token');
+        $paymentGateway->charge(2500, $token);
 
         // Verify that the charge was completed successfully
-        $lastCharge = $stripe->charges()->all(
+        $lastCharge = $stripe->charges->all(
             ['limit' => 1],
             ['api_key' => config('services.stripe.secret')]
         )['data'][0];
 
-        $this->assertEquals(2500, $lastCharge->amount)
+        $this->assertEquals(2500, $lastCharge->amount);
     } 
 }
