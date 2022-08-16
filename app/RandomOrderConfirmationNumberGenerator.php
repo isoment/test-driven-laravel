8787c1d1ac2d40376b82fcd057c6853408ed2e12
twill <twill1@gmx.com>
2022-08-15 22:23:21 -0500

    Section13.06

--- app/RandomOrderConfirmationNumberGenerator.php
+++ app/RandomOrderConfirmationNumberGenerator.php
@@ -2,10 +2,14 @@
 
 namespace App;
 
+use Illuminate\Support\Str;
+
 class RandomOrderConfirmationNumberGenerator implements OrderConfirmationNumberGenerator
 {
     public function generate() : string
     {
-        return str_repeat('A', 24);
+        $pool = '23456789ABCDEFGHJKLMNPQRSTUVWXYZ';
+
+        return substr(str_shuffle(str_repeat($pool, 24)), 0, 24);
     }
 }
