439294dc1aa60940c655ce3fa6eb6e54bf613de7
twill <twill1@gmx.com>
2022-10-01 21:28:35 -0500

    Section20.02

--- tests/Feature/Backstage/EditConcertTest.php
+++ tests/Feature/Backstage/EditConcertTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature\Backstage;
 
 use App\Models\Concert;
 use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
@@ -19,15 +20,17 @@ class EditConcertTest extends TestCase
         $user = User::factory()->create();
         $this->actingAs($user);
 
-        $concert = Concert::factory()->create([
+        $concert = Concert::factory()->unpublished()->create([
             'user_id' => $user->id
         ]);
 
         $this->assertFalse($concert->isPublished());
 
         $response = $this->get("/backstage/concerts/{$concert->id}/edit");
+
         $response->assertStatus(200);
-        $this->assertTrue($response->data('concert')->is($concert));
+        $concertFromResponse = $response->original->getData()['concert'];
+        $this->assertTrue($concertFromResponse->is($concert));
     }
 
     /**
@@ -38,7 +41,7 @@ class EditConcertTest extends TestCase
         $user = User::factory()->create();
         $this->actingAs($user);
 
-        $concert = Concert::factory()->create([
+        $concert = Concert::factory()->published()->create([
             'user_id' => $user->id
         ]);
 
@@ -99,8 +102,61 @@ class EditConcertTest extends TestCase
      */
     public function guests_are_asked_to_login_when_attempting_to_view_the_edit_form_for_a_concert_that_does_not_exist()
     {
-        $response = $this->get("/backstage/concerts/{$concert->id}/edit");
+        $response = $this->get("/backstage/concerts/9999/edit");
         $response->assertStatus(302);
         $response->assertRedirect('/login');
+    }
+
+    /**
+     *  @test
+     */
+    public function promoters_can_edit_their_own_unpublished_concerts()
+    {
+        $user = User::factory()->create();
+        $this->actingAs($user);
+
+        $concert = Concert::factory()->unpublished()->create([
+            'user_id' => $user->id,
+            'title' => 'Old title',
+            'subtitle' => 'Old subtitle',
+            'additional_information' => 'Old additional information',
+            'date' => Carbon::parse('2021-01-01 5:00pm'),
+            'venue' => 'Old venue',
+            'venue_address' => 'Old address',
+            'city' => 'Old city',
+            'state' => 'Old state',
+            'zip' => '00000',
+            'ticket_price' => 2000,
+        ]);
+
+        $this->assertFalse($concert->isPublished());
+
+        $response = $this->patch("/backstage/concerts/{$concert->id}", [
+            'title' => 'New title',
+            'subtitle' => 'New subtitle',
+            'additional_information' => 'New additional information',
+            'date' => '2022-02-02',
+            'time' => '8:00pm',
+            'venue' => 'New venue',
+            'venue_address' => 'New address',
+            'city' => 'New city',
+            'state' => 'New state',
+            'zip' => '99999',
+            'ticket_price' => '72.50',
+        ]);
+
+        $response->assertRedirect("/backstage/concerts");
+        tap($concert->fresh(), function($concert) {
+            $this->assertEquals('New title', $concert->title);
+            $this->assertEquals('New subtitle', $concert->subtitle);
+            $this->assertEquals('New additional information', $concert->additional_information);
+            $this->assertEquals(Carbon::parse('2022-02-02 8:00pm'), $concert->date);
+            $this->assertEquals('New venue', $concert->venue);
+            $this->assertEquals('New address', $concert->venue_address);
+            $this->assertEquals('New city', $concert->city);
+            $this->assertEquals('New state', $concert->state);
+            $this->assertEquals('99999', $concert->zip);
+            $this->assertEquals(7250, $concert->ticket_price);
+        });
     }
 }
