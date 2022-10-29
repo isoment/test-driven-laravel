4ee827edadd2792003653951da8f1ae9ca529013
twill <twill1@gmx.com>
2022-10-29 09:48:36 -0500

    Section28.04

--- tests/Unit/Jobs/ProcessPosterImageTest.php
+++ tests/Unit/Jobs/ProcessPosterImageTest.php
@@ -30,11 +30,42 @@ class ProcessPosterImageTest extends TestCase
 
         ProcessPosterImage::dispatch($concert);
 
+        // This is the image after it is processed
         $resizedImage = Storage::disk('public')->get('posters/example-poster.png');
 
         // Pull the width from the resized image and assign it to $width
-        list($width) = getimagesizefromstring($resizedImage);
+        list($width, $height) = getimagesizefromstring($resizedImage);
 
         $this->assertEquals(600, $width);
+        $this->assertEquals(776, $height);
+    }
+
+    /**
+     *  @test
+     */
+    public function it_optimizes_the_poster_image()
+    {
+        Storage::fake('public');
+
+        Storage::disk('public')->put(
+            'posters/example-poster.png', 
+            file_get_contents(base_path('tests/__fixtures__/small-unoptimized-poster.png'))
+        );
+
+        $concert = FactoryHelpers::createUnpublished([
+            'poster_image_path' => 'posters/example-poster.png'
+        ]);
+
+        ProcessPosterImage::dispatch($concert);
+
+        $optimizedImageSize = Storage::disk('public')->size('posters/example-poster.png');
+
+        $originalSize = filesize(base_path('tests/__fixtures__/small-unoptimized-poster.png'));
+
+        $this->assertLessThan($originalSize, $optimizedImageSize);
+
+        $optimizedImageContents = Storage::disk('public')->get('posters/example-poster.png');
+        $controlImageContents = file_get_contents(base_path('tests/__fixtures__/optimized-poster.png'));
+        $this->assertEquals($controlImageContents, $optimizedImageContents);
     }
 }
