022e3b382fb80936f25fd4cba10914d70a233a49
twill <twill1@gmx.com>
2022-11-02 20:15:59 -0500

    Section30.01

---
+++ tests/Browser/ConnectWithStripeTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Browser;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class ConnectWithStripeTest extends DuskTestCase
+{
+    /**
+     *  @test
+     */
+    public function connecting_a_stripe_account_successfully()
+    {
+        $user = User::factory()->create([
+            'stripe_account_id' => NULL,
+            'stripe_access_token' => NULL,
+        ]);
+
+        $this->browse(function (Browser $browser) use($user) {
+            $browser->loginAs($user)
+                    ->visit('/backstage/stripe-connect/authorize')
+                    ->assertUrlIs('https://connect.stripe.com/oauth/authorize')
+                    ->assertQueryStringHas('response_type', 'code')
+                    ->assertQueryStringHas('scope', 'read_write')
+                    ->assertQueryStringHas('client_id', config('services.stripe.client_id'));
+        });
+    }
+}
